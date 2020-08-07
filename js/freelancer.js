1f4edc38c8786c995277a4dbe053d21354b1cd40
Spencer Shen <spencer@spencerzshen.com>
2020-08-07 15:24:05 -0400

    clean up CSS, remove scrollspy from jQuery

--- js/freelancer.js
+++ js/freelancer.js
@@ -8,30 +8,20 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        var offsetValue = 90;
         $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top - 90
+          scrollTop: $($anchor.attr('href')).offset().top - offsetValue
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
 
-// Floating label headings for the contact form
-/*
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
-*/
-
 // Highlight the top nav as scrolling occurs
+/* Needs to be fixed to accommodate offset for the nav bar itself
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
+*/
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
